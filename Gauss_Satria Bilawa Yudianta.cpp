c7a192f75218b5d6a9ed13579a300232c485654f
Satria Bilawa Yudianta <tiobilawa@gmail.com>
2024-09-15 15:08:00 +0700

    Add files via upload

---
+++ Gauss_Satria Bilawa Yudianta.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Deklarasi variabel
+    float num1, num2, hasilKali, hasilBagi;
+
+    // Input angka pertama
+    cout << "Masukkan angka pertama: ";
+    cin >> num1;
+
+    // Input angka kedua
+    cout << "Masukkan angka kedua: ";
+    cin >> num2;
+
+    // Perkalian
+    hasilKali = num1 * num2;
+
+    // Pembagian
+    if (num2 != 0) {
+        hasilBagi = num1 / num2;
+    } else {
+        cout << "Pembagian dengan 0 tidak diperbolehkan." << endl;
+        return 1;
+    }
+
+    // Output hasil
+    cout << "Hasil perkalian: " << hasilKali << endl;
+    cout << "Hasil pembagian: " << hasilBagi << endl;
+
+    return 0;
+}
